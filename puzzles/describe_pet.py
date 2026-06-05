ada09ff3a98819374dda8cb40a9bb7656891fefe
DustinS93 <dustinsheldon10000@gmail.com>
2026-06-04 18:38:34 -0700

    First commit, define animal function, passing mutliple variables with one default

---
+++ puzzles/describe_pet.py
@@ -0,0 +1,5 @@
+def describe_pet(name, animal = "dog"): # CALLING MULITPLE ARGUMENTS, DEFAULT COMES SECOND
+    return "My pet " + animal + " is named " + name # ACCOUNT FOR SPACES
+
+
+print(describe_pet("Dustin"))
