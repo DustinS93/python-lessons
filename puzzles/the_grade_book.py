b3104bb2f6b7eb06516a53711551510128f30d39
DustinS93 <dustinsheldon10000@gmail.com>
2026-06-05 22:57:06 -0700

    grade book passing list through a function

---
+++ puzzles/the_grade_book.py
@@ -0,0 +1,17 @@
+def grade_book(grade_list):
+    for score in grade_list:
+        if score < 60:
+            print(str(score) + " - F")
+        elif score < 70:
+            print(str(score) + " - D")
+        elif score < 80:
+            print(str(score) + " - C") 
+        elif score < 90:
+            print(str(score) + " - B")
+        else:
+            print(str(score) + " - A")
+
+
+
+grades = [66, 77, 88, 99, 97, 75, 89]
+grade_book(grades)
