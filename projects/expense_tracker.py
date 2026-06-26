7332d7f40956c297c77ca5bc1f19c2291095d026
DustinS93 <dustinsheldon10000@gmail.com>
2026-06-25 22:15:13 -0700

    Concept: delete items with pop(index), mulitple save functions for fail safe

--- projects/expense_tracker.py
+++ projects/expense_tracker.py
@@ -1,4 +1,4 @@
-# Project two, Expense tracker Build V0.1
+# Project two, Expense tracker Build V0.2
 import os
 def load_expenses():
     if os.path.exists("expenses.txt"):
@@ -15,13 +15,18 @@ def save_expenses(expenses):
     with open("expenses.txt", "w") as f:
         for e in expenses:
             f.write(e["description"] + "," + e["category"] + "," + str(e["amount"]) + "\n")
+def print_list():
+    number = 0
+    for e in expenses:
+        number = number + 1
+        print(str(number) + ". " + e["description"] + " " + e["category"] + " " + str(e["amount"]))
 
 result = load_expenses()
 expenses = result
 
 while True:
     print("--- Menu ---")
-    print("[1] Add Expense [2] View All [3] View Totals [4] Quit")
+    print("[1] Add Expense [2] View All [3] View Totals [4] Delete [5] Quit")
     choice = int(input("Choose an option: "))
     if choice == 1:
         description = input("What did you buy?: ")
@@ -33,11 +38,9 @@ while True:
             print("Try Again:")
             continue
         expenses.append({"description": description, "category": category, "amount": amount})
+        save_expenses(expenses)
     elif choice == 2:
-        number = 0
-        for e in expenses:
-            number = number + 1
-            print(str(number) + ". " + e["description"] + " " + e["category"] + " " + str(e["amount"]))
+        print_list()
     elif choice == 3:
         total = {}
         for e in expenses:
@@ -46,8 +49,12 @@ while True:
             total[e["category"]] = total[e["category"]] + (e["amount"])
         for t in total.keys():
             print(f"{t.capitalize()}: ${total[t]:.2f}")
-
     elif choice == 4:
+        print_list()
+        n = input("Which item do you want to remove? ")
+        expenses.pop(int(n) - 1)
+        save_expenses(expenses)
+    elif choice == 5:
         print("Goodbye")
         break
 save_expenses(expenses)
